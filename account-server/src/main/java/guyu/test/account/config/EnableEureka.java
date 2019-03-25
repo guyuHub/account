cf54d8a56a41362ae018ee0cbb24e65c3142364f
guyu <1712473752@qq.com>
2019-03-25 17:37:11 +0800

    服务启动成功

--- account-server/src/main/java/guyu/test/account/config/EnableEureka.java
+++ account-server/src/main/java/guyu/test/account/config/EnableEureka.java
@@ -1,8 +1,16 @@
 package guyu.test.account.config;
 
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @EnableEurekaServer
+@Component
 public class EnableEureka {
+    @PostConstruct
+    public void pare(){
+        System.out.println("EnableEureka new in pare ");
+    }
 
 }
--- account-web/src/main/java/guyu/test/TradeWebApplication.java
+++ account-web/src/main/java/guyu/test/TradeWebApplication.java
@@ -3,7 +3,9 @@ package guyu.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {"guyu.test"}
+)
 public class TradeWebApplication {
 
 	public static void main(String[] args) {
